0142b37db50c7d6ba89f60815d12aee8feb4ad6c
Deivid Amadeus <deividamadeus1997@gmail.com>
2022-02-15 23:03:23 -0300

    removes dark theme

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -39,12 +39,6 @@ class MyApp extends StatelessWidget {
       onGenerateTitle: (BuildContext context) =>
           AppLocalizations.of(context)!.appTitle,
 
-      // Define a light and dark color theme. Then, read the user's
-      // preferred ThemeMode (light, dark, or system default) from the
-      // SettingsController to display the correct theme.
-      theme: ThemeData(),
-      darkTheme: ThemeData.dark(),
-
       // Define a function to handle named routes in order to support
       // Flutter web url navigation and deep linking.
       onGenerateRoute: (RouteSettings routeSettings) {
